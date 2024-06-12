6c137518a282b6a22b85bd1cbf3b3da10a91419c
Yefferson <vyefferson@gmail.com>
2024-06-12 15:45:46 -0500

    Finde sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera App en Angular';
+  // public counter: number = 10;
+
+  // increaseBy(value: number): void {
+  //     this.counter += value;
+
+  // }
+  // reset(): void {
+  //   this.counter = 10;
+  // }
+
 }
